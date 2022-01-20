8b7b3e94d8aa56e34e34edee7d53ab6f17553b6a
sijialouintel <sijia.lou@intel.com>
2022-01-20 10:34:04 +0000

    test

--- cicd.sh
+++ cicd.sh
@@ -1,7 +1,12 @@
-export PATH_REPO=/omniscidb
-export PATH_SCRIPTS=$PATH_REPO/scripts
-source $PATH_SCRIPTS/omnisci-env.sh
-bash  $PATH_REPO/build-omnisci-debug.sh
+mkdir build
+pwd
+cd build
+pwd
+
+# export PATH_REPO=/omniscidb
+# export PATH_SCRIPTS=$PATH_REPO/scripts
+# source $PATH_SCRIPTS/omnisci-env.sh
+# bash  $PATH_REPO/build-omnisci-debug.sh
 
 # cd $PATH_REPO/build-$BUILD_TYPE
 # ctest -C $BUILD_TYPE

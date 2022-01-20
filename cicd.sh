6c9ba3e083041c9b01939eef0079a9c9c07035bd
sijialouintel <sijia.lou@intel.com>
2022-01-20 13:26:43 +0000

    test2

--- cicd.sh
+++ cicd.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -l
 
-echo "here is the info: "
-mkdir build
-pwd
-cd build
-pwd
+# echo "here is the info: "
+# mkdir build
+# pwd
+# cd build
+# pwd
 
-# export PATH_REPO=/omniscidb
-# export PATH_SCRIPTS=$PATH_REPO/scripts
-# source $PATH_SCRIPTS/omnisci-env.sh
-# bash  $PATH_REPO/build-omnisci-debug.sh
+export PATH_REPO=/omniscidb
+export PATH_SCRIPTS=$PATH_REPO/scripts
+source $PATH_SCRIPTS/omnisci-env.sh
+bash  $PATH_REPO/build-omnisci-debug.sh
 
 # cd $PATH_REPO/build-$BUILD_TYPE
 # ctest -C $BUILD_TYPE
